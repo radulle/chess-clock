0101fdff511ff48150cb05f76140bd37d33bf099
radulle <radulle@gmail.com>
2021-05-22 13:46:39 +0200

    add all basic functionalities

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,169 +1,326 @@
-import { Timer } from '.'
+import { Status, Timer, TimerInterface } from '.'
 
 const TIME = 5 * 6000
 const UPDATE_INTERVAL = 100
 
+jest.useFakeTimers()
 const dateNow = jest.spyOn(Date, 'now')
+let time = 0
+dateNow.mockImplementation(() => (time += UPDATE_INTERVAL))
+
+const callback = jest.fn()
+
+const noIncrementOptions = {
+  stageList: [
+    {
+      time: [TIME, TIME],
+      increment: 0,
+    },
+  ],
+} as TimerInterface
 
 describe('Timer', () => {
   beforeEach(() => {
-    jest.useFakeTimers()
-  })
-
-  afterEach(() => {
-    jest.clearAllTimers()
     jest.clearAllMocks()
+    jest.clearAllTimers()
+    time = 0
   })
 
-  describe('Tracks moves', () => {
-    it('Tracks moves of two players', () => {
+  describe('Basic', () => {
+    it('can track moves of two players', () => {
       const timer = new Timer()
-      expect(timer.move).toStrictEqual([0, 0])
+      expect(timer.state.move).toStrictEqual([0, 0])
     })
 
-    it('Adds a move', () => {
+    it('can track times of two players', () => {
       const timer = new Timer()
-      timer.logMove(0)
-      expect(timer.move).toStrictEqual([1, 0])
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME])
     })
 
-    it('Current move defines current stage', () => {
-      const timer = new Timer({ stageList: [{ time: [1, 1], move: 1 }] })
-      timer.logMove(0)
-      expect(timer.stage).toStrictEqual([1, 0])
-    })
-  })
-
-  describe('Tracks time', () => {
-    it('Tracks times of two players', () => {
+    it('tracks history of two players', () => {
       const timer = new Timer()
-      expect(timer.remainingTime).toStrictEqual([TIME, TIME])
+      expect(timer.state.log).toStrictEqual([[], []])
     })
 
-    it('Initial times can differ', () => {
+    it('can have different initial times', () => {
       const timer = new Timer({ stageList: [{ time: [4, 2] }] })
-      expect(timer.remainingTime).toStrictEqual([4, 2])
+      expect(timer.state.remainingTime).toStrictEqual([4, 2])
     })
 
-    it('Add time to a player', () => {
+    it('can add a move', () => {
+      const timer = new Timer()
+      timer.push(0)
+      expect(timer.state.move).toStrictEqual([1, 0])
+    })
+
+    it('stage defined by move', () => {
       const timer = new Timer({
-        stageList: [{ time: [4, 2] }, { time: [4, 2], move: 1 }],
+        stageList: [{ time: [1, 1] }, { time: [1, 1], move: 1 }],
       })
-      timer.addTime(0, 1)
-      expect(timer.remainingTime).toStrictEqual([5, 2])
+      timer.push(0)
+      expect(timer.state.stage.map((e) => e.i)).toStrictEqual([1, 0])
     })
 
-    it('Add time to a player on beginning of a stage', () => {
+    it('can add time', () => {
+      const timer = new Timer()
+      timer.addTime(0, 42)
+      expect(timer.state.remainingTime).toStrictEqual([TIME + 42, TIME])
+    })
+
+    it('will add time on new stage', () => {
       const timer = new Timer({
-        stageList: [{ time: [4, 2] }, { time: [4, 2], move: 1 }],
+        stageList: [{ time: [42, 21] }, { time: [42, 21], move: 1 }],
       })
-      timer.addTimeOnNewStage(0)
-      expect(timer.remainingTime).toStrictEqual([4, 2])
-      timer.logMove(0)
-      timer.addTimeOnNewStage(0)
-      expect(timer.remainingTime).toStrictEqual([8, 2])
+      timer.push(0)
+      expect(timer.state.remainingTime).toStrictEqual([84, 21])
+    })
+
+    it('can reset to initial values', () => {
+      const timer = new Timer()
+      timer.push(0)
+      timer.addTime(0, 42)
+      expect(timer.state.move).toStrictEqual([1, 0])
+      expect(timer.state.remainingTime).toStrictEqual([TIME + 42, TIME])
+      expect(timer.state.log).not.toStrictEqual([[], []])
+      timer.reset()
+      expect(timer.state.move).toStrictEqual([0, 0])
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME])
+      expect(timer.state.log).toStrictEqual([[], []])
+    })
+
+    it('provides state', () => {
+      const timer = new Timer()
+      expect(timer.state).not.toBe(undefined)
+    })
+
+    it('status is ready initially and after reset', () => {
+      const timer = new Timer()
+      expect(timer.state.status).toBe(Status.ready)
+      timer.push(0)
+      expect(timer.state.status).toBe(Status.live)
+      timer.reset()
+      expect(timer.state.status).toBe(Status.ready)
+    })
+
+    it('status is done when remainingTime has expired', () => {
+      const timer = new Timer()
+      timer.push(0)
+      jest.runTimersToTime(TIME)
+      expect(timer.state.status).toBe(Status.done)
+    })
+
+    it('remainingTime can not be lower than 0', () => {
+      const timer = new Timer({ callback })
+      timer.push(0)
+      jest.runTimersToTime(2 * TIME)
+      expect(timer.state.remainingTime).toStrictEqual([TIME, 0])
     })
   })
 
-  describe('Tracks history', () => {
-    it('Tracks history of two players', () => {
+  describe('Push', () => {
+    it('can starts timing', () => {
       const timer = new Timer()
-      expect(timer.log).toStrictEqual([[], []])
+      expect(timer.state.status).toBe(Status.ready)
+      timer.push(0)
+      expect(timer.state.status).toBe(Status.live)
     })
 
-    it('Push to history', () => {
+    it('can set last player', () => {
       const timer = new Timer()
-      timer.logHistory(0, 42)
-      expect(timer.log).toStrictEqual([[42], []])
-    })
-  })
-
-  it('Reset move, time and history to initial values', () => {
-    const timer = new Timer()
-    timer.logMove(0)
-    timer.addTime(0, 1)
-    timer.logHistory(0, 0)
-    expect(timer.move).not.toStrictEqual([0, 0])
-    expect(timer.remainingTime).not.toStrictEqual(timer.stageList[0].time)
-    expect(timer.log).not.toStrictEqual([[], []])
-    timer.reset()
-    expect(timer.move).toStrictEqual([0, 0])
-    expect(timer.remainingTime).toStrictEqual(timer.stageList[0].time)
-    expect(timer.log).toStrictEqual([[], []])
-  })
-
-  describe('Trigger', () => {
-    it('Trigger starts timing', () => {
-      const timer = new Timer()
-      expect(timer.timestamp).toBeUndefined()
-      timer.trigger(0)
-      expect(timer.timestamp).not.toBeUndefined()
+      timer.push(0)
+      expect(timer.state.lastPlayer).toBe(0)
     })
 
-    it('Trigger sets last player', () => {
+    it('can add a move to player', () => {
       const timer = new Timer()
-      timer.trigger(0)
-      expect(timer.lastPlayer).toBe(0)
+      timer.push(0)
+      expect(timer.state.move).toStrictEqual([1, 0])
     })
 
-    it('Trigger adds a move to player', () => {
+    it('has no effect if twice in a row by the same player', () => {
       const timer = new Timer()
-      timer.trigger(0)
-      expect(timer.move).toStrictEqual([1, 0])
+      timer.push(0)
+      timer.push(0)
+      expect(timer.state.move).toStrictEqual([1, 0])
     })
 
-    it('Same player can not trigger twice in a row', () => {
-      const timer = new Timer()
-      timer.trigger(0)
-      timer.trigger(0)
-      expect(timer.move).toStrictEqual([1, 0])
-    })
+    it('can update elapsed time (without increment)', () => {
+      const timer = new Timer(noIncrementOptions)
 
-    it('Subsequent trigger sets new timestamp (without increment)', () => {
-      let time = 0
-      dateNow.mockImplementation(() => (time += UPDATE_INTERVAL))
+      timer.push(0)
+      expect(timer.state.timestamp).toBe(100)
 
-      const timer = new Timer({ increment: 0 })
-
-      timer.trigger(0)
-      expect(timer.timestamp).toBe(100)
-
-      timer.trigger(1)
-      expect(timer.timestamp).toBe(200)
+      timer.push(1)
+      expect(timer.state.timestamp).toBe(200)
 
       expect(dateNow).toBeCalledTimes(2)
     })
 
-    it('Log time on non opening moves (without increment)', () => {
-      let time = 0
-      dateNow.mockImplementation(() => (time += UPDATE_INTERVAL))
+    it('can log time on non opening moves (without increment)', () => {
+      const timer = new Timer(noIncrementOptions)
 
-      const timer = new Timer({ increment: 0 })
+      expect(timer.state.status).toBe(Status.ready)
 
-      timer.trigger(0)
-      expect(timer.log[0][0]).toBeUndefined()
-      expect(timer.remainingTime).toStrictEqual([TIME, TIME])
+      timer.push(0)
+      expect(timer.state.log[0][0]).toBe(undefined)
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME])
 
       jest.runTimersToTime(1000)
-      expect(timer.remainingTime).toStrictEqual([TIME, TIME - 1000])
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME - 1000])
 
-      timer.trigger(1)
-      expect(timer.log[1][0]).toBe(1100)
-      expect(timer.remainingTime).toStrictEqual([TIME, TIME - 1100])
+      timer.push(1)
+      expect(timer.state.log[1][0]).toBe(1100)
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME - 1100])
 
       jest.runTimersToTime(450)
-      timer.trigger(0)
-      expect(timer.log[0][0]).toBe(500)
-      expect(timer.remainingTime).toStrictEqual([TIME - 500, TIME - 1100])
+      timer.push(0)
+      expect(timer.state.log[0][0]).toBe(500)
+      expect(timer.state.remainingTime).toStrictEqual([TIME - 500, TIME - 1100])
 
+      expect(timer.state.status).toBe(Status.live)
       expect(dateNow).toBeCalledTimes(17)
     })
   })
-})
 
-// Handle Pause ...
-// Handle Increments
-// Handle Done ...
-// Provide State ...
-// Clamp RemainingTime ...
-// Add callbacks ...
+  describe('Pause', () => {
+    it('has effect if status is live', () => {
+      const timer = new Timer(noIncrementOptions)
+
+      timer.push(0)
+
+      expect(timer.state.status).toBe(Status.live)
+      timer.pause()
+
+      jest.runTimersToTime(4200)
+
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME - 100])
+      expect(timer.state.log[1][0]).toBe(100)
+      expect(dateNow).toBeCalledTimes(2)
+      expect(timer.state.status).toBe(Status.paused)
+    })
+
+    it('has no effect if status is not live', () => {
+      const timer = new Timer(noIncrementOptions)
+
+      // ready
+      timer.pause()
+      jest.runTimersToTime(4200)
+      expect(timer.state.status).toBe(Status.ready)
+
+      timer.push(0)
+      jest.runTimersToTime(TIME)
+      expect(timer.state.status).toBe(Status.done)
+
+      // done
+      timer.pause()
+      jest.runTimersToTime(4200)
+      expect(timer.state.status).toBe(Status.done)
+
+      expect(dateNow).toBeCalledTimes(301)
+    })
+  })
+
+  describe('Resume', () => {
+    it('has effect if status is paused', () => {
+      const timer = new Timer(noIncrementOptions)
+
+      timer.push(0)
+
+      timer.pause()
+
+      jest.runTimersToTime(1000)
+
+      timer.resume()
+
+      jest.runTimersToTime(1000)
+
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME - 1100])
+      expect(timer.state.log[1][0]).toBe(1100)
+      expect(dateNow).toBeCalledTimes(13)
+      expect(timer.state.status).toBe(Status.live)
+    })
+
+    it('has no effect if status is not live', () => {
+      const timer = new Timer(noIncrementOptions)
+
+      // ready
+      timer.resume()
+      jest.runTimersToTime(1000)
+      expect(timer.state.status).toBe(Status.ready)
+
+      timer.push(0)
+      jest.runTimersToTime(1000)
+
+      // live
+      timer.resume()
+      jest.runTimersToTime(1000)
+      expect(timer.state.remainingTime).toStrictEqual([TIME, TIME - 2000])
+      expect(dateNow).toBeCalledTimes(21)
+      expect(timer.state.status).toBe(Status.live)
+
+      jest.runTimersToTime(TIME)
+
+      // done
+      timer.resume()
+      expect(timer.state.status).toBe(Status.done)
+    })
+
+    it('Resume has no effect if status is ready or done', () => {
+      const timer = new Timer(noIncrementOptions)
+
+      // ready
+      timer.resume()
+      expect(timer.state.status).toBe(Status.ready)
+
+      timer.push(0)
+      jest.runTimersToTime(TIME)
+      expect(timer.state.status).toBe(Status.done)
+      expect(timer.state.remainingTime).toStrictEqual([TIME, 0])
+
+      // done
+      timer.resume()
+      jest.runTimersToTime(1000)
+      expect(timer.state.status).toBe(Status.done)
+      expect(timer.state.remainingTime).toStrictEqual([TIME, 0])
+    })
+  })
+
+  describe('Callback', () => {
+    it('is called on push', () => {
+      const timer = new Timer({ callback })
+      timer.push(0)
+      expect(callback).toBeCalledTimes(1)
+    })
+
+    it('is called on pause', () => {
+      const timer = new Timer({ callback })
+      timer.push(0)
+      timer.pause()
+      timer.pause()
+      expect(callback).toBeCalledTimes(2)
+    })
+
+    it('is called on resume', () => {
+      const timer = new Timer({ callback })
+      timer.push(0)
+      timer.resume()
+      timer.pause()
+      timer.resume()
+      timer.resume()
+      expect(callback).toBeCalledTimes(3)
+    })
+
+    it('is called on addTime', () => {
+      const timer = new Timer({ callback })
+      timer.addTime(0, 0)
+      expect(callback).toBeCalledTimes(1)
+    })
+
+    it('is called on ticks', () => {
+      const timer = new Timer({ callback })
+      timer.push(0)
+      jest.runTimersToTime(1000)
+      expect(callback).toBeCalledTimes(11)
+    })
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-enum Increment {
+export enum Increment {
   'Delay',
   'Bronstein',
   'Fischer',
 }
 
-enum Status {
+export enum Status {
   'ready',
   'live',
   'paused',
@@ -15,146 +15,169 @@ const TIME = 5 * 6000
 const INCREMENT = 5
 const INCREMENT_TYPE = Increment.Fischer
 const UPDATE_INTERVAL = 100
+const STAGE_LIST = [
+  {
+    time: [TIME, TIME] as [number, number],
+    incrementType: INCREMENT_TYPE,
+    increment: INCREMENT,
+  },
+]
 
-interface State {
+interface Stage {
+  i?: number
+  time: [number, number]
+  move?: number
+  increment?: number
+  incrementType?: Increment
+}
+
+export interface State {
   move: [number, number]
   remainingTime: [number, number]
   lastPlayer?: 0 | 1
   log: [number[], number[]]
   status: Status
-  stage: [number, number]
+  stage: [Stage, Stage]
+  timestamp?: number
 }
 
-interface Timer {
-  stageList: { time: [number, number]; move?: number }[]
-  increment: number
-  incrementType: Increment
-  updateInterval: number
+export interface TimerInterface {
+  stageList?: Stage[]
+  updateInterval?: number
   callback?: (state: State) => void
 }
 
-class Timer {
-  move: [number, number] = [0, 0]
-  remainingTime: [number, number]
-  lastPlayer?: 0 | 1
-  log: [number[], number[]] = [[], []]
-  status: Status = Status.ready
+export class Timer {
+  #move: [number, number] = [0, 0]
+  #remainingTime: [number, number]
+  #lastPlayer?: 0 | 1
+  #log: [number[], number[]] = [[], []]
+  #status: Status = Status.ready
+  #stage: [Stage, Stage]
 
-  timestamp?: number
-  interval?: NodeJS.Timeout
+  #stageList: Stage[]
+  #updateInterval: number
+  #callback?: (state: State) => void
+
+  #timestamp?: number
+  #interval?: NodeJS.Timeout
 
   constructor({
-    stageList = [{ time: [TIME, TIME] }],
-    increment = INCREMENT,
-    incrementType = INCREMENT_TYPE,
+    stageList = STAGE_LIST,
     updateInterval = UPDATE_INTERVAL,
     callback,
-  }: Partial<Timer> = {}) {
-    this.stageList = stageList
-    this.increment = increment
-    this.incrementType = incrementType
-    this.updateInterval = updateInterval
-    this.callback = callback
+  }: TimerInterface = {}) {
+    this.#stageList = stageList.map((e, i) => ({ i, ...e }))
+    this.#stage = [this.#stageList[0], this.#stageList[0]]
+    this.#remainingTime = [...this.#stageList[0].time]
 
-    this.remainingTime = [...stageList[0].time]
+    this.#updateInterval = updateInterval
+    this.#callback = callback
   }
 
   reset() {
-    this.log = [[], []]
-    this.move = [0, 0]
-    this.remainingTime = [...this.stageList[0].time]
-    this.lastPlayer = undefined
-    this.status = Status.ready
+    this.#log = [[], []]
+    this.#move = [0, 0]
+    this.#remainingTime = [...this.#stageList[0].time]
+    this.#lastPlayer = undefined
+    this.#status = Status.ready
+    this._callback()
   }
 
   pause() {
-    if (this.interval) clearInterval(this.interval)
-    this.tick(this.otherPlayer)
-    this.status = Status.paused
+    if (this.#status !== Status.live) return
+    if (this.#interval) clearInterval(this.#interval)
+    this._record(this._otherPlayer)
+    this.#status = Status.paused
+    this._callback()
   }
 
   resume() {
-    this.timestamp = Date.now()
-    this.interval = setInterval(() => {
-      this.tick(this.otherPlayer)
-    }, this.updateInterval)
-    this.status = Status.live
-  }
-
-  logMove(player: 0 | 1) {
-    this.move[player]++
+    if (this.#status !== Status.paused) return
+    this.#timestamp = Date.now()
+    this.#interval = setInterval(() => {
+      this._tick(this._otherPlayer)
+    }, this.#updateInterval)
+    this.#status = Status.live
+    this._callback()
   }
 
   addTime(player: 0 | 1, time: number) {
-    this.remainingTime[player] += time
+    this._addTime(player, time)
+    this._callback()
   }
 
-  logHistory(player: 0 | 1, time: number) {
-    this.log[player].push(time)
+  _addTime(player: 0 | 1, time: number) {
+    this.#remainingTime[player] += time
   }
 
-  addTimeOnNewStage(player: 0 | 1) {
-    const newStage = this.stageList.find((e) => e.move === this.move[player])
-    if (newStage) this.addTime(player, newStage.time[player])
-  }
+  push(player: 0 | 1) {
+    if (this.#status === Status.done) return
+    if (this.#lastPlayer === player) return
+    if (this.#interval) clearInterval(this.#interval)
 
-  tick(player: 0 | 1) {
-    if (this.callback) this.callback(this.state)
-    if (this.status === Status.done) return
-    if (this.status !== Status.live) this.status = Status.live
-    const timestamp = this.timestamp
-    this.timestamp = Date.now()
-    if (timestamp !== undefined) {
-      const elapsed = this.timestamp - timestamp
-      this.log[player][this.log[player].length - 1] += elapsed
-      this.remainingTime[player] -= elapsed
-      if (this.remainingTime[player] <= 0) {
-        this.remainingTime[player] = 0
-        this.status = Status.done
-      }
-    }
-  }
+    this._tick(player)
 
-  trigger(player: 0 | 1) {
-    if (this.status === Status.done) return
-    if (this.lastPlayer === player) return
-    if (this.interval) clearInterval(this.interval)
+    this.#lastPlayer = player
+    this._logMove(player)
+    this._updateStage(player)
+    this.#log[this._otherPlayer].push(0)
 
-    this.tick(player)
-
-    this.lastPlayer = player
-    this.logMove(player)
-    this.addTimeOnNewStage(player)
-    this.log[this.otherPlayer].push(0)
-
-    this.interval = setInterval(() => {
-      this.tick(this.otherPlayer)
-    }, this.updateInterval)
-  }
-
-  get otherPlayer() {
-    return this.lastPlayer === 1 ? 0 : 1
-  }
-
-  get stage() {
-    return this.move.map(
-      (move) =>
-        this.stageList.findIndex(
-          ({ move: moves }) => move >= (moves || Infinity)
-        ) + 1
-    ) as [number, number]
+    this.#interval = setInterval(() => {
+      this._tick(this._otherPlayer)
+    }, this.#updateInterval)
   }
 
   get state(): State {
     return {
-      remainingTime: [...this.remainingTime],
-      move: [...this.move],
-      stage: [...this.stage],
-      lastPlayer: this.lastPlayer,
-      log: this.log.map((e) => [...e]) as [number[], number[]],
-      status: this.status,
+      remainingTime: this.#remainingTime,
+      move: this.#move,
+      stage: this.#stage,
+      lastPlayer: this.#lastPlayer,
+      log: this.#log,
+      status: this.#status,
+      timestamp: this.#timestamp,
     }
   }
-}
 
-export { Timer }
+  private _callback() {
+    if (this.#callback) this.#callback(this.state)
+  }
+
+  private _logMove(player: 0 | 1) {
+    this.#move[player]++
+  }
+
+  private _updateStage(player: 0 | 1) {
+    const newStage = this.#stageList.find((e) => e.move === this.#move[player])
+    if (newStage) {
+      this._addTime(player, newStage.time[player])
+      this.#stage[player] = newStage
+    }
+  }
+
+  private _record(player: 0 | 1) {
+    const timestamp = this.#timestamp
+    this.#timestamp = Date.now()
+    if (timestamp !== undefined) {
+      const elapsed = this.#timestamp - timestamp
+      this.#log[player][this.#log[player].length - 1] += elapsed
+      this.#remainingTime[player] -= elapsed
+      if (this.#remainingTime[player] <= 0) {
+        this.#remainingTime[player] = 0
+        this.#status = Status.done
+      }
+    }
+  }
+
+  private _tick(player: 0 | 1) {
+    if (this.#status === Status.done) return
+    if (this.#status !== Status.live) this.#status = Status.live
+    this._record(player)
+    this._callback()
+  }
+
+  private get _otherPlayer() {
+    return this.#lastPlayer === 1 ? 0 : 1
+  }
+}
